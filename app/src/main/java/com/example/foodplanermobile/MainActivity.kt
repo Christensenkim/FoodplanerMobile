81b8a2077fa81075388204a29ec1c715b5d81328
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-05-10 22:46:10 +0200

    asd

--- app/src/main/java/com/example/foodplanermobile/MainActivity.kt
+++ app/src/main/java/com/example/foodplanermobile/MainActivity.kt
@@ -2,21 +2,32 @@ package com.example.foodplanermobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.foodplanermobile.model.WeekDTO
 import com.example.foodplanermobile.services.FoodplanerService
 import io.socket.client.IO
+import io.socket.client.Socket
 
 class MainActivity : AppCompatActivity() {
+
+    var mSocket: Socket? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val app: FoodplanerService = application as FoodplanerService
-        val mSocket = app.getMSocket()
+        mSocket = app.getMSocket()
         mSocket?.connect()
 
+        Toast.makeText(this, mSocket?.id() + "",Toast.LENGTH_SHORT).show()
+
+
+    }
+
+    fun Tester(view: View) {
+        mSocket?.emit("test")
         Toast.makeText(this, mSocket?.id() + "",Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/foodplanermobile/services/FoodplanerService.kt
+++ app/src/main/java/com/example/foodplanermobile/services/FoodplanerService.kt
@@ -5,7 +5,7 @@ import io.socket.client.IO
 import io.socket.client.Socket
 import java.net.URISyntaxException
 
-private const val URL = "https://foodplanner-backend.herokuapp.com/"
+private const val URL = "http://10.0.2.2:3000"
 
 class FoodplanerService : Application() {
     private var mSocket: Socket? = null
