c9965d00024e83c686c8d4641aa8b37190d90b66
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-05-10 11:46:35 +0200

    added the list

--- app/src/main/java/com/example/foodplanermobile/MainActivity.kt
+++ app/src/main/java/com/example/foodplanermobile/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.foodplanermobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listOfWeeks: ListView = findViewById(R.id.listOfWeeks)
     }
 }
