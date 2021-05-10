242e004ad533bbe17530824ff4245ca3764e5756
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-05-10 21:49:05 +0200

    Backend is getting a signal

--- app/src/main/java/com/example/foodplanermobile/MainActivity.kt
+++ app/src/main/java/com/example/foodplanermobile/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.foodplanermobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.foodplanermobile.model.WeekDTO
+import com.example.foodplanermobile.services.FoodplanerService
+import io.socket.client.IO
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val app: FoodplanerService = application as FoodplanerService
+        val mSocket = app.getMSocket()
+        mSocket?.connect()
+
+        Toast.makeText(this, mSocket?.id() + "",Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/foodplanermobile/services/FoodplanerService.kt
+++ app/src/main/java/com/example/foodplanermobile/services/FoodplanerService.kt
@@ -1,4 +1,25 @@
 package com.example.foodplanermobile.services
 
-class FoodplanerService {
+import android.app.Application
+import io.socket.client.IO
+import io.socket.client.Socket
+import java.net.URISyntaxException
+
+private const val URL = "https://foodplanner-backend.herokuapp.com/"
+
+class FoodplanerService : Application() {
+    private var mSocket: Socket? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        try {
+            mSocket = IO.socket(URL)
+        } catch (e: URISyntaxException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    fun getMSocket(): Socket? {
+        return mSocket
+    }
 }
