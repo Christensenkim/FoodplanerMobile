854ac29f85d3cb0f8e7db36e32c61c1c0d4542ff
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2021-05-12 08:56:26 +0200

    Ja

--- app/src/main/java/com/example/foodplanermobile/MainActivity.kt
+++ app/src/main/java/com/example/foodplanermobile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.foodplanermobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -30,5 +31,10 @@ class MainActivity : AppCompatActivity() {
     fun Tester(view: View) {
         mSocket?.emit("test")
         Toast.makeText(this, mSocket?.id() + "",Toast.LENGTH_SHORT).show()
+    }
+
+    fun MealOverview(view: View) {
+        val intent = Intent(this, MealOverviewActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/foodplanermobile/model/BEMeal.kt
@@ -0,0 +1,4 @@
+package com.example.foodplanermobile.model
+
+class BEMeal(var id: Number, var name: String, var ingredients: String, var directions: String, var description: String) {
+}
