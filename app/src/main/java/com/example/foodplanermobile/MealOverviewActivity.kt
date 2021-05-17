57846336c0da155abb621f07be656f821938c008
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-05-17 21:53:29 +0200

    meals now come from backend

--- app/src/main/java/com/example/foodplanermobile/MealOverviewActivity.kt
+++ app/src/main/java/com/example/foodplanermobile/MealOverviewActivity.kt
@@ -11,32 +11,50 @@ import android.view.ViewGroup
 import android.widget.*
 import com.example.foodplanermobile.model.BEMeal
 import com.example.foodplanermobile.model.Meal
+import com.example.foodplanermobile.model.WeekDto
+import com.example.foodplanermobile.services.FoodplanerService
+import com.example.foodplanermobile.services.adapters.WeekOverviewAdapter
+import com.google.gson.Gson
+import io.socket.client.Socket
+import org.json.JSONArray
+import java.util.ArrayList
 
 class MealOverviewActivity : ListActivity() {
 
     var selectedMeal: BEMeal? = null
-    var allMeals: Array<BEMeal>? = null
+    var allMeals: ArrayList<BEMeal>? = null
+
+    var mSocket: Socket? = null
+    val gson: Gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        allMeals = Meal.getAll()
-        listAdapter = MealAdapter(this, allMeals!!)
 
+        val foodplanerService: FoodplanerService = application as FoodplanerService
+        mSocket = foodplanerService.getMSocket()
+        mSocket?.connect()
+
+        mSocket?.on("allMeals") { args ->
+            if (args[0] != null){
+                var data = args[0] as JSONArray
+                var mealsDB: ArrayList<BEMeal> = ArrayList()
+                mealsDB.clear()
+                for (i in 0 until data.length()) {
+                    val meal = data[i]
+                    val mealdto = gson.fromJson(meal.toString(), BEMeal::class.java)
+                    mealsDB.add(mealdto)
+                }
+                allMeals = mealsDB
+            }
+            runOnUiThread{
+                listAdapter = MealAdapter(this, allMeals!!)
+            }
+        }
+        mSocket?.emit("getMeals")
     }
 
     override fun onListItemClick(parent: ListView?,
                                  v: View?, position: Int, id: Long) {
-//        // position is in the list!
-//        // first get the name of the person clicked
-//        val name = Meal().getAll()[position].name
-//
-//        // and a greeting
-//        Toast.makeText(
-//                this,
-//                "Hi $name! Have you done your homework?",
-//                Toast.LENGTH_LONG
-//        ).show()
-
         val intent = Intent(this, MealDetailsActivity::class.java)
         selectedMeal = allMeals!![position]
         if (selectedMeal != null) {
@@ -44,11 +62,10 @@ class MealOverviewActivity : ListActivity() {
             Toast.makeText(this, "You have clicked ${selectedMeal!!.name}", Toast.LENGTH_LONG).show()
             startActivity(intent)
         }
-
     }
 }
 
-internal class MealAdapter(context: Context, private val meals: Array<BEMeal>) : ArrayAdapter<BEMeal>(context, 0, meals)
+internal class MealAdapter(context: Context, private val meals: ArrayList<BEMeal>) : ArrayAdapter<BEMeal>(context, 0, meals)
 {
     private val colours = intArrayOf(
             Color.parseColor("#AAAAAA"),
--- app/src/main/java/com/example/foodplanermobile/model/BEMeal.kt
+++ app/src/main/java/com/example/foodplanermobile/model/BEMeal.kt
@@ -2,5 +2,9 @@ package com.example.foodplanermobile.model
 
 import java.io.Serializable
 
-class BEMeal(var id: Number, var name: String, var ingredients: String, var directions: String, var description: String): Serializable {
+class BEMeal(var id: Number,
+             var name: String,
+             var ingredients: String,
+             var directions: String,
+             var description: String): Serializable {
 }
